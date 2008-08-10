1d382e5ea45941bc88dbc188d997be2d78db4cd6
tao.takashi <tao.takashi@localhost>
2008-08-10 19:55:18 +0000

    added init() here

--- pyogp/lib/agentdomain/__init__.py
+++ pyogp/lib/agentdomain/__init__.py
@@ -1,36 +1,6 @@
-from zope.component import getUtility, getMultiAdapter
-from zope.interface import implements
-from pyogp.lib.base.interfaces import ICredentialDeserializer
-from interfaces import ILoginHandler
-from pyogp.lib.agentdomain.interfaces import IAgent
+from zope.configuration.xmlconfig import xmlconfig
 
-class Agent(object):
-    implements(IAgent)
-    
-    def __init__(self,credentials):
-        self.credentials
-    
-
-def login(context, payload):
-    """login an agent"""
-    deserializer = getUtility(ICredentialDeserializer)
-    credentials = deserializer.deserialize(payload)
-    
-    # now try to get the user from the host system
-    login_handler = getMultiAdapter((credentials,context), ILoginHandler)
-    agent = login_handler.login()
-    
-    # now cast it to IAgent
-    agent = IAgent(agent)
-    return agent
-    
-    
-    # TOOD: do not assume that it's always plain password
-    
-    firstname = payload['firstname'].lower()
-    lastname = payload['lastname'].lower()
-    password = payload['password']
-    username = "%s-%s" %(firstname,lastname)
-    user = self.context.get(username,None)
-    
-    
+def init():
+    from pkg_resources import resource_stream
+    fp = resource_stream(__name__, 'configure.zcml')
+    xmlconfig(fp, testing=False)
