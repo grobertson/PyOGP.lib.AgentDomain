1ad0e124f240b5bd3d59ee19f26147204368e901
tao.takashi <tao.takashi@localhost>
2008-08-10 19:55:05 +0000

    moved init() to __init__

--- pyogp/lib/agentdomain/registration.py
+++
@@ -1,6 +0,0 @@
-from zope.configuration.xmlconfig import xmlconfig
-
-def init():
-    from pkg_resources import resource_stream
-    fp = resource_stream(__name__, 'configure.zcml')
-    xmlconfig(fp, testing=False)
